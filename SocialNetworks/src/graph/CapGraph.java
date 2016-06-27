bfadbb172fb890591af59ffa72ca2d80ec906d99
unsalted-almonds <google@gmail.com>
2016-06-26 20:21:55 -0500

    solution

--- SocialNetworks/src/graph/CapGraph.java
+++ SocialNetworks/src/graph/CapGraph.java
@@ -3,9 +3,13 @@
  */
 package graph;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
 
 /**
  * @author Your name here.
@@ -19,10 +23,14 @@ public class CapGraph implements Graph {
 	/* (non-Javadoc)
 	 * @see graph.Graph#addVertex(int)
 	 */
+	
+	// use adjacency list/set 
+	HashMap<Integer, HashSet<Integer>> adjList = new HashMap<Integer, HashSet<Integer>>();
+	
 	@Override
 	public void addVertex(int num) {
-		// TODO Auto-generated method stub
-
+		if (!adjList.containsKey(num))
+			adjList.put(num, new HashSet<Integer>());
 	}
 
 	/* (non-Javadoc)
@@ -30,8 +38,11 @@ public class CapGraph implements Graph {
 	 */
 	@Override
 	public void addEdge(int from, int to) {
-		// TODO Auto-generated method stub
 
+		if (adjList.containsKey(from))
+			adjList.get(from).add(to);
+		else
+			adjList.put(from, new HashSet<Integer>());
 	}
 
 	/* (non-Javadoc)
@@ -39,8 +50,29 @@ public class CapGraph implements Graph {
 	 */
 	@Override
 	public Graph getEgonet(int center) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		CapGraph res = new CapGraph();
+		
+		if (this.adjList.containsKey(center)){						
+			res.addVertex(center);
+			// add all vertices and edges from center to them
+			for(Integer i : this.listEdgeFromVertex(center)){
+				res.addVertex(i);
+				res.addEdge(center, i);
+			}
+			// add all edges inter-connected 
+			for (Integer i : res.listVerices()){
+				// skip center 
+				if ( !i.equals(center)){					
+					for (Integer p : this.listEdgeFromVertex(i)){						
+						if (res.listVerices().contains(p))
+							res.addEdge(i, p);						
+					}					
+				}
+			}			
+		}
+		
+		return res;
 	}
 
 	/* (non-Javadoc)
@@ -49,7 +81,12 @@ public class CapGraph implements Graph {
 	@Override
 	public List<Graph> getSCCs() {
 		// TODO Auto-generated method stub
-		return null;
+		
+		Stack<Integer> verticesStack = this.stackifyVertices(this.listVerices());
+		
+		verticesStack = this.graphTraversal(this, verticesStack);
+				
+		return this.graphTraversalSecond(this.transposeGraph(this), verticesStack);
 	}
 
 	/* (non-Javadoc)
@@ -57,8 +94,110 @@ public class CapGraph implements Graph {
 	 */
 	@Override
 	public HashMap<Integer, HashSet<Integer>> exportGraph() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.adjList;
+	}
+	
+	public Set<Integer> listVerices(){
+		return this.adjList.keySet();
+	}
+	
+	public Set<Integer> listEdgeFromVertex(int num){
+		Set<Integer> res = null;
+		
+		if (this.adjList.containsKey(num))
+			res = this.adjList.get(num);
+		
+		return res;
+	}
+	
+	
+	private Stack<Integer> graphTraversal(CapGraph g, Stack<Integer> verticesStack){
+
+		Set<Integer> visited = new HashSet<Integer>();
+		Stack<Integer> finished = new Stack<Integer>();
+		
+		while (!verticesStack.isEmpty()){			
+			Integer vertex = verticesStack.pop();			
+			if (!visited.contains(vertex))
+				graphTraversal(g, vertex, visited ,finished);						
+		}
+		
+		return finished;
 	}
 
+	private List<Graph> graphTraversalSecond(CapGraph g, Stack<Integer> verticesStack){
+
+		Set<Integer> visited = new HashSet<Integer>();
+		Stack<Integer> finished = new Stack<Integer>();
+		
+		List<Graph> res = new ArrayList<Graph>();
+		
+		while (!verticesStack.isEmpty()){			
+			Integer vertex = verticesStack.pop();			
+			if (!visited.contains(vertex)){
+				graphTraversal(g, vertex, visited ,finished);
+				res.add(subGraph(g, finished));
+			}			
+		}
+		
+		return res;
+	}
+	
+	private void graphTraversal(CapGraph g, Integer vertex, Set<Integer> visited ,Stack<Integer> finished){
+		
+		visited.add(vertex);
+		
+		for (Integer neighbor : g.listEdgeFromVertex(vertex)){
+			
+			if (!visited.contains(neighbor))
+				graphTraversal(g, neighbor, visited ,finished);
+		}		
+		finished.push(vertex);		
+	}
+	
+	private CapGraph transposeGraph(CapGraph g) {
+
+		CapGraph res = new CapGraph();
+
+		for (Integer i : g.listVerices()) {			
+			res.addVertex(i);			
+			for (Integer p : g.listEdgeFromVertex(i)){
+				res.addVertex(p);
+				res.addEdge(p, i);
+			}
+		}
+		return res;
+	}
+	
+	private Stack<Integer> stackifyVertices(Set<Integer> vertices){
+		Stack<Integer> res = new Stack<Integer>();
+		for (Integer i : vertices)
+			res.push(i);
+		return res;
+	}
+	
+	// create sub graph of the orignal graph using given vertices 
+	private CapGraph subGraph(CapGraph g, Stack<Integer> vertices){
+		CapGraph res = new CapGraph();
+		
+		while (!vertices.isEmpty()){		
+			Integer vertex = vertices.pop();
+			res.addVertex(vertex);
+			for (Integer neighbor : g.listEdgeFromVertex(vertex)){
+				if (vertices.contains(neighbor))
+					res.addEdge(vertex, neighbor);
+			}
+		}
+		/*
+		for (Integer vertex : vertices){
+			res.addVertex(vertex);
+			for (Integer neighbor : g.listEdgeFromVertex(vertex)){
+				if (vertices.contains(neighbor))
+					res.addEdge(vertex, neighbor);
+			}
+		}*/
+		
+		return res;
+	}
+	
 }
